744895d9483a46a72088a654de1474f801e76df2
Domitille Rémy <domitille.remy@gmail.com>
2024-07-29 13:12:58 +0000

    finance day

---
+++ models/mart/finance_days.sql
@@ -0,0 +1,10 @@
+SELECT
+    date_date,
+    COUNT(orders_id) AS nb_transactions,
+    SUM(order_revenue) AS revenue,
+    ROUND(COUNT(orders_id)/SUM(order_revenue),2) AS avg_basket,
+    SUM(margin) AS daily_margin,
+    SUM(operational_margin) AS daily_operational_margin
+FROM {{ref("int_orders_operational") }}
+GROUP BY
+    date_date
